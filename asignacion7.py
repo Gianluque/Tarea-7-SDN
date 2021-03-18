ced1f67d4a78a5a8801a7369469192bb946d5e5f
Gianluque <12-11412@usb.ve>
2021-03-18 14:17:01 -0400

    Commit para generar conflicto

--- asignacion7.py
+++ asignacion7.py
@@ -1,11 +1,10 @@
 import requests
 from pprint import pprint
-
+# Realiza request al token y luego lo usa como header para obtener una lista
 response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
-pprint(payload)
 response = requests.get(
     'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
     headers={'X-Auth-Token': payload['Token']})
