4bdfc65a8dc4ac8eb28310b33c0932cf312a2852
Gianluque <12-11412@usb.ve>
2021-03-18 00:26:59 -0400

    Codigo V2

--- asignacion7.py
+++ asignacion7.py
@@ -6,5 +6,15 @@ response = requests.post(
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
 pprint(payload)
+response = requests.get(
+    'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+    headers={'X-Auth-Token': payload['Token']})
+Dict = response.json()['response']
+nameList = []
+for i in range(len(Dict)):
+	
+	nameList.append([Dict[i]['family'],Dict[i]['hostname'],
+                    Dict[i]['apManagerInterfaceIp'],Dict[i]['lastUpdated'],
+                    Dict[i]['reachabilityStatus']])
+pprint(nameList)
 
-# hola
