828d55260e357ac1648e5e515a309c785fc5fb7f
Gianluque <12-11412@usb.ve>
2021-03-18 14:15:20 -0400

    Commit para generar conflicto

--- asignacion7.py
+++ asignacion7.py
@@ -1,6 +1,6 @@
 import requests
 from pprint import pprint
-
+# Realiza un request e imprime el token recibido
 response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
